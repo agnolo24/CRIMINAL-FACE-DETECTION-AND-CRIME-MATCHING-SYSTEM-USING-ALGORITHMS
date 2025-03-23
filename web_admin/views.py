4dc3e736ca01d157d371bcb66c83a9b88f76131d
Amal jith <agnolo2024@example.com>
2025-03-23 10:25:48 +0530

    solved some bugs

--- web_admin/views.py
+++ web_admin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from police_station . models import police_station_registration
 from staff . models import staff
+from gust . models import login
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ from staff . models import staff
 # This is the function for showing the details of the police station we registred (used for admin)
 def police_station_details_table(request):
     police_station_data = police_station_registration.objects.all()
-    return render(request, 'web_admin/data.html',{'Datas':police_station_data})
+    return render(request, 'web_admin/data.html',{'data1':police_station_data})
 
 # This is the function for showing the details of the staff we registred (used for admin)
 def staff_details_table(request):
